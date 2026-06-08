a6a5dddf48ca185399802dc41964f02df247f0f8
Gambin007 <schertenleib.noe@gmail.com>
2026-06-08 22:51:14 +0200

    Initial Commit

---
+++ MiniCaddie/MiniCaddieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCaddieApp.swift
+//  MiniCaddie
+//
+//  Created by Noé Schertenleib on 08.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MiniCaddieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
